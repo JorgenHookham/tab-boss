5d5fa0caf1aa7fd8e4a2826c215211d56cffe698
Jorgen Hookham <Jorgen@Hookham.co>
2018-03-16 20:51:30 -0700

    remove unused var; move some code around

--- popup.js
+++ popup.js
@@ -4,9 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	var checkbox = document.getElementById('checkbox');
 	var number = document.getElementById('number');
 	var text = document.getElementById('text');
-	var activeTab = null;
-	var allTabs = null;
-	var bg = chrome.extension.getBackgroundPage();
 
 	function tabBossAPI (request) {
 		return new Promise(
@@ -24,52 +21,52 @@ document.addEventListener('DOMContentLoaded', () => {
 		number.value = state.tabCycleIntervalSeconds;
 		text.value = state.webSocketURL;
 
-		chrome.tabs.getSelected(null, (tab) => {
-			activeTab = tab;
-		});
-
-		chrome.tabs.getAllInWindow(null, (tabs) => {
-			allTabs = tabs;
-			tabs.forEach((tab) => {
-				var option = document.createElement('option');
-				option.textContent = tab.title;
-				option.value = tab.id;
-				option.selected = tab.id == activeTab.id;
-				dropdown.appendChild(option);
-			});
-		});
-
-		dropdown.onchange = (e) => {
-			chrome.tabs.update(parseInt(dropdown.value), {active: true});
-		};
-
-		checkbox.onchange = (e) => {
-			tabBossAPI({
-				method: 'setTabCycleActive',
-				props: {
-					isActive: checkbox.checked
-				}
-			});
-		};
-
-		number.onchange = (e) => {
-			tabBossAPI({
-				method: 'setTabCycleIntervalSeconds',
-				props: {
-					seconds: number.value
-				}
-			});
-		};
-
-		text.onchange = (e) => {
-			tabBossAPI({
-				method: 'setTabBossWebSocketURL',
-				props: {
-					webSocketURL: text.value
-				}
-			});
-		};
-
 	});
+
+	chrome.tabs.getSelected(null, (tab) => {
+		activeTab = tab;
+	});
+
+	chrome.tabs.getAllInWindow(null, (tabs) => {
+		allTabs = tabs;
+		tabs.forEach((tab) => {
+			var option = document.createElement('option');
+			option.textContent = tab.title;
+			option.value = tab.id;
+			option.selected = tab.id == activeTab.id;
+			dropdown.appendChild(option);
+		});
+	});
+
+	dropdown.onchange = (e) => {
+		chrome.tabs.update(parseInt(dropdown.value), {active: true});
+	};
+
+	checkbox.onchange = (e) => {
+		tabBossAPI({
+			method: 'setTabCycleActive',
+			props: {
+				isActive: checkbox.checked
+			}
+		});
+	};
+
+	number.onchange = (e) => {
+		tabBossAPI({
+			method: 'setTabCycleIntervalSeconds',
+			props: {
+				seconds: number.value
+			}
+		});
+	};
+
+	text.onchange = (e) => {
+		tabBossAPI({
+			method: 'setTabBossWebSocketURL',
+			props: {
+				webSocketURL: text.value
+			}
+		});
+	};
 
 });
